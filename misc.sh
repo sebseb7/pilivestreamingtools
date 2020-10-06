116c23c2fdd593ebcf337897425f5af574ad03dc
sebseb7 <sebgreenbus@gmail.com>
2020-10-06 19:37:34 +0100

    fix

--- misc.sh
+++ misc.sh
@@ -1,6 +1,9 @@
 sudo apt-get install wiringpi
 sudo raspi-config nonint do_memory_split 256
-echo "dtoverlay=pi3-disable-wifi" | sudo tee -a /boot/config.txt
-echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
+sudo sed /boot/config.txt -i -e "s/^dtoverlay=disable-wifi//"
+sudo sed /boot/config.txt -i -e "s/^dtoverlay=disable-bt//"
+echo "dtoverlay=disable-wifi" | sudo tee -a /boot/config.txt
+echo "dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
+sudo sed /boot/config.txt -i -e "s/^dtparam=audio=om/dtparam=audio=off/"
 sudo systemctl disable hciuart
 #raspi-config noint enable_overlayfs
