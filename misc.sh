c62ce8f1fe6fb91e93dac79c79f250488c84c2e1
sebseb7 <sebgreenbus@gmail.com>
2020-10-06 20:21:54 +0200

    disable built-in wifi

--- misc.sh
+++ misc.sh
@@ -1,2 +1,5 @@
 raspi-config nonint do_memory_split 256
+echo "dtoverlay=pi3-disable-wifi" | sudo tee -a /boot/config.txt
+echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
+sudo systemctl disable hciuart
 #raspi-config noint enable_overlayfs
