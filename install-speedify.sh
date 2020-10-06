ed3a3395a845f5fcec19092294d59c45bde160f2
sebseb7 <sebgreenbus@gmail.com>
2020-10-06 19:16:46 +0100

    speedify

---
+++ install-speedify.sh
@@ -0,0 +1,10 @@
+curl https://get.speedify.com/pgp.key | sudo apt-key add
+sudo sh -c 'echo deb http://apt.connectify.me/ speedify main > /etc/apt/sources.list.d/connectify.list'
+sudo apt-get update
+sudo apt-get install speedify
+/usr/share/speedify/speedify_cli startupconnect on
+/usr/share/speedify/speedify_cli encryption off
+/usr/share/speedify/speedify_cli mode streaming
+/usr/share/speedify/speedify_cli login <login> <password>
+/usr/share/speedify/speedify_cli connect closest
+
