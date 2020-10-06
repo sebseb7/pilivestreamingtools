151b786e58651811dd25262cea6ac5acbcc8cb3c
sebseb7 <sebgreenbus@gmail.com>
2020-10-06 19:24:26 +0100

    wiringpi

--- misc.sh
+++ misc.sh
@@ -1,4 +1,5 @@
-raspi-config nonint do_memory_split 256
+sudo apt-get install wiringpi
+sudo raspi-config nonint do_memory_split 256
 echo "dtoverlay=pi3-disable-wifi" | sudo tee -a /boot/config.txt
 echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
 sudo systemctl disable hciuart
