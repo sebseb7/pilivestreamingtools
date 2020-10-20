5c5267af5030d744a13c9cd0a686c503cb146529
sebseb7 <sebgreenbus@gmail.com>
2020-10-20 04:57:25 +0100

    improvement

--- install-speedify.sh
+++ install-speedify.sh
@@ -1,11 +1,26 @@
+read -p "AP Name:"
+
+sed speedify.conf -i -e "s/^WIFI_SSID.*$//"
+echo "WIFI_SSID=\"$REPLY\"" >> speedify.conf
+
+read -p "AP Password (8 characters):"
+
+sed speedify.conf -i -e "s/^WIFI_PASSWORD.*$//"
+echo "WIFI_PASSWORD=\"$REPLY\"" >> speedify.conf
+
+read -p "Speedify login:"
+sp_login=$REPLY
+read -p "Speedify password:"
+sp_pass=$REPLY
+
 curl https://get.speedify.com/pgp.key | sudo apt-key add
 sudo sh -c 'echo deb http://apt.connectify.me/ speedify main > /etc/apt/sources.list.d/connectify.list'
 sudo apt-get update
-sudo apt-get install speedify
+sudo apt-get -y install speedify
 /usr/share/speedify/speedify_cli startupconnect on
 /usr/share/speedify/speedify_cli encryption off
 /usr/share/speedify/speedify_cli mode streaming
-/usr/share/speedify/speedify_cli login <login> <password>
+/usr/share/speedify/speedify_cli login $sp_login $sp_pass
 /usr/share/speedify/speedify_cli connect closest
 sudo cp speedify.conf /etc/speedify
 sudo service speedify-sharing restart
