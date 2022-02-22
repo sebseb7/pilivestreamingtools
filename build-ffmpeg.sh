192066f4f1ffac5b1ad14b94442e55b68ff006b8
sebseb7 <sebgreenbus@gmail.com>
2022-02-22 21:22:56 +0000

    srt

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -4,7 +4,7 @@ wget https://github.com/lgeek/FFmpeg/commit/10a7ccaceb56cd4ed1275a155900fb6087fe
 cat 10a7ccaceb56cd4ed1275a155900fb6087fe9f56.diff | patch -p1
 wget https://github.com/lgeek/FFmpeg/commit/4fcb83f2aa2b1d2e6ad528687cf159c167b98b18.diff
 cat 4fcb83f2aa2b1d2e6ad528687cf159c167b98b18.diff | patch -p1
-./configure --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree --enable-mmal --extra-ldflags=-latomic --arch=armel --target-os=linux
+./configure --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree --enable-mmal --enable-libsrt --extra-ldflags=-latomic --arch=armel --target-os=linux
 make -j4
 sudo make install
 cd ..
